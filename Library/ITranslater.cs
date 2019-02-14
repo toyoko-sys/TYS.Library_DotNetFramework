adc30f45c13742011048d7b1f63230c5d2ad8078
takeda.hiromitu <takeda.hiromitu@toyoko-sys.co.jp>
2019-02-14 15:24:36 +0900

    ルーターの登録方法を変更 ・辞書を廃止してキャッシュ化した

---
+++ Library/ITranslater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TYS.Library.Domain.Translater;
+
+namespace TYS.Library
+{
+    public interface ITranslater
+    {
+        Task<bool> Execute(TranslaterArgs args);
+    }
+
+    public static class TranslaterCash<T>
+        where T : ITranslater, new()
+    {
+        private static ITranslater cash = new T();
+        public static TranslaterFunction func;
+
+        static TranslaterCash()
+        {
+            func = cash.Execute;
+        }
+    }
+}
